e63a52e91a8846bbc0dae095dc7340f32699920f
Hien Le <lnqhien@gmail.com>
2018-03-22 00:19:23 -0700

    Add CORS for web clients.

--- src/main/java/com/gmail/lnqhien/todo/Application.java
+++ src/main/java/com/gmail/lnqhien/todo/Application.java
@@ -2,11 +2,39 @@ package com.gmail.lnqhien.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	/**
+	 * Enable CORS so browser clients can make requests.
+	 * 
+	 * Adapted from https://www.programcreek.com/java-api-examples/?code=BakkerTom/happy-news/happy-news-master/api/src/main/java/nl/fhict/happynews/api/AuthorizationServerConfig.java
+	 */
+	@Bean
+	FilterRegistrationBean<CorsFilter> corsFilter() {
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", config);
+
+	    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+	    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+	    return bean;
 	}
 }
